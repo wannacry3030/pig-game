8e0e7714d83a83b9729cffd888962f98d0e1caf8
wannacry3030 <wannacry.bat@proton.me>
2023-04-05 16:21:11 -0300

    att

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+//criando variaveis, selecionando os elementos
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1'); //para multiplos elementos
+
+//especificando
+score0El.textContent = 0;
+score1El.textContent = 0;
