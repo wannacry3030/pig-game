5a8b9667409eb99751d56665e2a5a53e738516a5
wannacry3030 <wannacry.bat@proton.me>
2023-04-05 16:23:18 -0300

    att

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
 //criando variaveis, selecionando os elementos
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1'); //para multiplos elementos
+const diceEl = document.querySelector('.dice');
 
-//especificando
+//especificando a condição inicial
 score0El.textContent = 0;
 score1El.textContent = 0;
+diceEl.classList.add('hidden');
